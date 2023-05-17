144c58ddb74a670d51ca500e2a8b8ad998f6dc31
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-05-17 19:50:19 +0200

    work on docker for aoi and database

---
+++ srcs/backend/initBack.sh
@@ -0,0 +1,17 @@
+echo "LANCEMENT DU SCIPT"
+
+# npm install
+# npx prisma init --datasource-provider postgres 
+# npx prisma migrate dev --name init
+
+
+
+
+
+# npm install 
+# npm insstall -g @nestjs/cli --save-dev 
+# export PATH="$PATH:$(npm bin -g)" 
+# npx prisma db push
+# npx prisma generate
+# npx prisma db seed
+# npm run start:dev
--- srcs/backend/initDB.sh
+++
@@ -1,3 +0,0 @@
-//init DB
-npx prisma init --datasource-provider postgres 
-npx prisma migrate dev --name init
