5d0a41a572661173d8be7bdbd88465053e62cc6a
ko <lheurtev@e2r10p6.clusters.42paris.fr>
2023-05-22 16:00:26 +0200

    prisma test

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -1,17 +1,20 @@
 echo "LANCEMENT DU SCRIPT"
 
- npm install
- #npx prisma init --datasource-provider postgres
- npx prisma migrate dev --name init
+#npm install
+#npx prisma init --datasource-provider postgres
+#npx prisma migrate dev --name init#
 
 
 
 
 
  #npm install -g @nestjs/cli --save-dev
- npm install @prisma/client prisma
- export PATH="$PATH:$(npm bin -g)"
- npx prisma db push
- npx prisma generate
+# npm install @prisma/client prisma
+# export PATH="$PATH:$(npm bin -g)"
+# npx prisma db push
+# npx prisma generate
  #npx prisma db seed
  #npm run start:dev
+
+npm install prisma @prisma/client
+npx prisma migrate dev --name init
