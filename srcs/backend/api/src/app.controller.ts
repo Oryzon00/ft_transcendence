b5de79de911f573804e8ce0a1168022ae90d2b5e
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-05-25 18:28:53 +0200

    work on controllers // prisma studio is launched with nest

--- srcs/backend/api/src/app.controller.ts
+++ srcs/backend/api/src/app.controller.ts
@@ -27,15 +27,11 @@ export class AppController {
 export class AppController2 {
 	constructor(private readonly appService: AppService) {}
 
-	
-	// @Get()
-	// async getHello(): Promise<User[]> {
-	// 	return await this.appService.getHello();
-	// }
 	@Get()
 	async getTest2(): Promise<String> {
 		return "test2";
 	}
+
 	@Get("test3")
 	async getTest3(): Promise<String> {
 		return "test3";
--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -1,10 +1,15 @@
 npm install
 
 #set up database a parti du shema prisma
+
 npx prisma migrate dev --name init
+# migrate vs db push ?
+
+npx prisma generate
 
 #Start nest
-npm run start:dev 
+npm run start:dev &
+npx prisma studio
 
 # pour avoir prisma studio en localhost:5555
 #	docker exec -it api bash --> npx prisma studio 
