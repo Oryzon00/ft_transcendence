cc14899691deeca88062c8ee05040e81aefd8595
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-05-26 16:19:11 +0200

    new script clean for init api

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -1,9 +1,10 @@
+rm -rf dist/ migrations/ nodes_modules/
+
 npm install
 
-#set up database a parti du shema prisma
-
+# # migrate vs db push ?
 npx prisma migrate dev --name init
-# migrate vs db push ?
+# # npx db push
 
 npx prisma generate
 
@@ -11,5 +12,3 @@ npx prisma generate
 npm run start:dev &
 npx prisma studio
 
-# pour avoir prisma studio en localhost:5555
-#	docker exec -it api bash --> npx prisma studio 
