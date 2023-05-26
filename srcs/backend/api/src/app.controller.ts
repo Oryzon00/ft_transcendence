976ebe317ddeb6a56535025d174d62f70db086f5
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-05-26 16:19:32 +0200

    work on nestJS

--- srcs/backend/api/src/app.controller.ts
+++ srcs/backend/api/src/app.controller.ts
@@ -37,17 +37,17 @@ export class AppController {
 	
 }
 
-// @Controller("test2")
-// export class AppController2 {
-// 	constructor(private readonly appService: AppService) {}
+@Controller("test2")
+export class AppController2 {
+	constructor(private readonly appService: AppService) {}
 
-// 	@Get()
-// 	async getTest2(): Promise<String> {
-// 		return "test2";
-// 	}
+	@Get()
+	async getTest2(): Promise<String> {
+		return "test2";
+	}
 
-// 	@Get("test3")
-// 	async getTest3(): Promise<String> {
-// 		return "test3";
-// 	}
-// }
+	@Get("test3")
+	async getTest3(): Promise<String> {
+		return "test3";
+	}
+}
