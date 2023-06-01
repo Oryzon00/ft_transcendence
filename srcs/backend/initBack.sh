ccb273e607f55d0e98dcdb44fc2af61b9f3d04e7
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-06-01 16:30:09 +0200

    add id42 in schema

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -3,10 +3,12 @@ rm -rf dist/ nodes_modules/ src/prisma/migrations/
 npm install
 
 # migrate vs db push ?
+echo migrations
 npx prisma migrate dev --name init
 
 # npx prisma generate
 
 #Start nest
 npm run start:dev &
+sleep 5 &&
 npx prisma studio
