cdbd98fe417c824d7247417140d25cdb32f97c95
Dassy OUATTARA <douattar@paul-f4Br3s6.clusters.42paris.fr>
2023-06-20 10:23:41 +0200

    backend works

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -2,9 +2,9 @@ rm -fr dist/ nodes_modules/
 npm install
 
 # npx prisma db push --accept-data-loss
-npx prisma migrate dev --name init
+# npx prisma migrate dev --name init
 npx prisma generate
 
 #Start nest
-npm run start:dev &
-npx prisma studio
+npm run start:dev
+# npx prisma studio
