ab798360c8db97335156af8f467d3a9d904737cf
Dassy OUATTARA <douattar@paul-f4Br3s6.clusters.42paris.fr>
2023-06-22 09:30:08 +0200

    try to run without prisma studio

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -1,10 +1,13 @@
 rm -fr dist/ nodes_modules/
 npm install
 
-# npx prisma db push --accept-data-loss
-# npx prisma migrate dev --name init
-npx prisma generate
+# migrate vs db push ?
+echo migrations
+npx prisma migrate dev --name init
+
+# npx prisma generate
 
 #Start nest
-npm run start:dev
-# npx prisma studio
+npm run start:dev & (
+sleep 5 &&
+npx prisma studio)
