3483602b791533056b0debe90b43461c6963d042
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-04 15:48:50 +0200

    fix bug prisma adrian

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -1,9 +1,8 @@
 rm -rf dist/ nodes_modules/
 npm install
 
-npx prisma format
 # npx prisma db push --accept-data-loss
-# npx prisma migrate dev --name init
+npx prisma migrate dev --name init
 npx prisma generate
 
 #Start nest
