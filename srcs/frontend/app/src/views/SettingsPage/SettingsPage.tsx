98ba50bb523506cb69edb953715e12ae089da309
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-11 20:38:10 +0200

    clean again

--- srcs/frontend/app/src/views/SettingsPage/SettingsPage.tsx
+++ srcs/frontend/app/src/views/SettingsPage/SettingsPage.tsx
@@ -1,16 +1,8 @@
-import User from "../../components/Home/User";
 import SettingsLayout from "../../layouts/SettingsLayout/SettingsLayout";
-import useUser from "../../utils/hooks/useUser";
-
 import { UserProvider } from "../../utils/contexts/userContext";
 
 function SettingsPage() {
-	// const userHook = useUser();
-
 	return (
-		// <UserContext.Provider value={userHook}>
-		// 	<SettingsLayout />
-		// </UserContext.Provider>
 		<UserProvider>
 			<SettingsLayout />
 		</UserProvider>
