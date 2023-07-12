b1745197f5d07c220eb6c7d59250b261946a1cbe
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-12 18:08:36 +0200

    React>StrictMode in comment

--- srcs/frontend/app/src/main.tsx
+++ srcs/frontend/app/src/main.tsx
@@ -49,8 +49,10 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
+	// <React.StrictMode>
+	<>
 		<ToastContainer />
 		<RouterProvider router={router} />
-	</React.StrictMode>
+	</>
+	// </React.StrictMode>
 );
