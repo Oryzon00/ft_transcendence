41defa4026acc6bbdca70113956100de02ab4275
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-14 16:37:32 +0200

    clean home

--- srcs/frontend/app/src/layouts/HomeLayout/HomeLayout.tsx
+++ srcs/frontend/app/src/layouts/HomeLayout/HomeLayout.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import ButtonSettings from "../../components/Home/Button2FA.tsx";
 import User from "../../components/Home/User.tsx";
 import ProfileButton from "../../components/Home/ProfileButton.tsx";
-import './HomeLayout.styles.css'
+import "./HomeLayout.styles.css";
 import { cookieProtection } from "../../utils/cookieProtection.ts";
 
 function HomeLayout() {
 	cookieProtection();
 
-	const [count, setCount] = useState(0);
 	return (
 		<div className="home-root">
 			<div className="profile-button">
@@ -17,11 +15,6 @@ function HomeLayout() {
 			</div>
 			<Outlet />
 			<div className="main-site">
-				<div className="card">
-					<button onClick={() => setCount((count) => count + 1)}>
-						truc {count}
-					</button>
-				</div>
 				<div>Welcome home !</div>
 				<User></User>
 				<ButtonSettings />
