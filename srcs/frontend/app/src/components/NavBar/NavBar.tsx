3c51eee357e032ecd830b0d6cac91043c075923c
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-14 17:11:01 +0200

    start to work on NavBar

---
+++ srcs/frontend/app/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+export function NavBar() {
+	return (
+		<nav>
+			<a href="/">HOME</a>
+			<ul>
+				<li>
+					<a href="/play">PLAY</a>
+				</li>
+				<li>
+					<a href="/leaderboard">LEADERBOARD</a>
+				</li>
+				<li>
+					<a href="/chat">CHAT</a>
+				</li>
+			</ul>
+			<div>SEARCHBAR</div>
+			<div>USER PROFILE NAV BAR</div>
+		</nav>
+	);
+}
