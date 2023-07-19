4bc693b47a8204c844793eba27e4f3e843326ae2
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-19 19:25:47 +0200

    fix bug odal // delte bootstrap c de la merde

--- srcs/frontend/app/src/components/NavBar/NavBar.tsx
+++ srcs/frontend/app/src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function NavBar() {
 	return (
-		<nav classname="navbar navbar-expand-lg">
+		<nav >
 			<Link to="/home">HOME</Link>
 			<ul>
 				<Link to="/home">HOME</Link>
