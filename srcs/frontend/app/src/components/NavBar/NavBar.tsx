8a5c8841e825ffc14c13a4132652c8d395734b36
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-19 18:20:51 +0200

    clean prettier and navbar WIP

--- srcs/frontend/app/src/components/NavBar/NavBar.tsx
+++ srcs/frontend/app/src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function NavBar() {
 	return (
-		<nav>
+		<nav classname="navbar navbar-expand-lg">
 			<Link to="/home">HOME</Link>
 			<ul>
 				<Link to="/home">HOME</Link>
