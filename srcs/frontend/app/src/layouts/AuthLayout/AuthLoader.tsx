3d98bb18d0633aa2924b73cb74eefb7bebfb936a
ko <lheurtev@paul-f4Ar7s1.clusters.42paris.fr>
2023-07-24 16:31:48 +0200

    protection null in auth

--- srcs/frontend/app/src/layouts/AuthLayout/AuthLoader.tsx
+++ srcs/frontend/app/src/layouts/AuthLayout/AuthLoader.tsx
@@ -33,6 +33,7 @@ export async function authLoader() {
 			if (data.access_token) {
 				document.cookie = `JWT=${data.access_token};Path=/`;
 				self.location.replace("home");
+				return null;
 			}
 			throw new Error("Unexpected error has occured");
 		})
