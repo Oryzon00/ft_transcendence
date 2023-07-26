c6ca15ada9fd6c638a4ef7cffb23db6d5dd98360
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-26 16:44:28 +0200

    can import env in frontend

--- srcs/frontend/app/src/utils/api42Address.ts
+++ srcs/frontend/app/src/utils/api42Address.ts
@@ -1,8 +1,5 @@
-const api42Address = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-6df58ea5af6397af7e840febffebf3d4bddf67673df569097d3ac72fb76a919f&redirect_uri=http://${window.location.hostname}:8000/auth&response_type=code`;
-
-// @ts-nocheck
-// console.log("check");
-// console.log(process.env);
-// const api42Address = `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.API42_UID!}&redirect_uri=http://${window.location.hostname}:8000/auth&response_type=code`;
+const api42Address = `https://api.intra.42.fr/oauth/authorize?client_id=${
+	import.meta.env.VITE_API42_UID
+}&redirect_uri=http://${window.location.hostname}:8000/auth&response_type=code`;
 
 export default api42Address;
