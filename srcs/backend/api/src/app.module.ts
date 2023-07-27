8760ead30312fc0a4a3ba2a3899fe6b62c13241f
Dassy OUATTARA <douattar@bess-f4r5s8.clusters.42paris.fr>
2023-07-27 15:00:12 +0200

    end of the merge

--- srcs/backend/api/src/app.module.ts
+++ srcs/backend/api/src/app.module.ts
@@ -4,22 +4,21 @@ import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { PrismaModule } from "./prisma/prisma.module";
-<<<<<<< HEAD
-import { ChatModule } from "./chat/chat.module";
+import { GameModule } from "./game/game.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-	imports: [AuthModule, UserModule, PrismaModule, ChatModule],
-=======
-import { GameModule } from './game/game.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-
-@Module({
-	imports: [AuthModule, UserModule, PrismaModule, ServeStaticModule.forRoot({
-		rootPath: join(__dirname, '..', 'images'),
-		serveRoot: '/images',
-	}), GameModule,],
->>>>>>> origin/main
+	imports: [
+		AuthModule,
+		UserModule,
+		PrismaModule,
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, "..", "images"),
+			serveRoot: "/images"
+		}),
+		GameModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
