b45df2906848f75e2555b740c24f81ba6856282c
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-07-28 17:19:55 +0200

    try to fix bug image resize

--- srcs/frontend/app/tailwind.config.js
+++ srcs/frontend/app/tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+// @type {import('tailwindcss').Config}
 export default {
-	content: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.tsx"],
+	content: ["./src/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {}
 	},
