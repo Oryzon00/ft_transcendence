d3247c0ba95737b9cd6d1aa55685f3c2d4229204
Adrian <adrian.mael.jung@gmail.com>
2023-08-20 00:33:53 +0200

    oops this components is still in use

--- srcs/frontend/app/src/components/Home/ProfileButton.tsx
+++ srcs/frontend/app/src/components/Home/ProfileButton.tsx
@@ -1,14 +1,14 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-// function ProfileButton () {
-// 	const navigate = useNavigate();
-// 	function goTo() {
-// 		if (self.location.href === `http://${window.location.hostname}:8000/home`)
-// 			navigate("/profile")
-// 		else
-// 			navigate("/home")
-// 	}
-// 	return <button onClick={goTo}>Profile</button>
-// }
+function ProfileButton () {
+	const navigate = useNavigate();
+	function goTo() {
+		if (self.location.href === `http://${window.location.hostname}:8000/home`)
+			navigate("/profile")
+		else
+			navigate("/home")
+	}
+	return <button onClick={goTo}>Profile</button>
+}
 
-// export default ProfileButton;
+export default ProfileButton;
