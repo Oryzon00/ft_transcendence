30bf0f4b2316b6bd89d8b59859b977f244f62e64
Adrian <adrian.mael.jung@gmail.com>
2023-08-19 02:28:21 +0200

    fix bug navbar

--- srcs/frontend/app/src/components/NavBar/HomeNavLink.tsx
+++ srcs/frontend/app/src/components/NavBar/HomeNavLink.tsx
@@ -2,34 +2,37 @@ import { NavLink } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 
 export function HomeNavLink() {
-	let activeClassName = "w-56 px-2 py-2 rounded-md shrink-0";
-	let normalClassName = "w-56 px-2 py-2 rounded-md shrink-0";
+	let activeClassName = " px-2 py-2 rounded-md shrink-0";
+	let normalClassName = " px-2 py-2 rounded-md shrink-0";
 
 	return (
-		<NavLink
-			to="/home"
-			className={({ isActive }) =>
-				isActive ? activeClassName : normalClassName
-			}
-		>
-			{({ isActive }) =>
-				isActive ? (
-					<div className="p-0.5 w-fit h-fit rounded-md">
-						<AiFillHome
-							className="box-border mx-2 my-2 h-11 w-11"
-							color="#92400e"
-							title="HomeActive"
-						/>
-					</div>
-				) : (
-					<div className="hover:bg-amber-800 p-0.5 w-fit h-fit rounded-md">
-						<AiFillHome
-							className="box-border mx-2 my-2 h-11 w-11"
-							title="HomeInactive"
-						/>
-					</div>
-				)
-			}
-		</NavLink>
+		<div className="flex">
+			<NavLink
+				to="/home"
+				className={({ isActive }) =>
+					isActive ? activeClassName : normalClassName
+				}
+			>
+				{({ isActive }) =>
+					isActive ? (
+						<div className="p-0.5 w-fit h-fit rounded-md">
+							<AiFillHome
+								className="box-border mx-2 my-2 h-11 w-11"
+								color="#92400e"
+								title="HomeActive"
+							/>
+						</div>
+					) : (
+						<div className="hover:bg-amber-800 p-0.5 w-fit h-fit rounded-md">
+							<AiFillHome
+								className="box-border mx-2 my-2 h-11 w-11"
+								title="HomeInactive"
+							/>
+						</div>
+					)
+				}
+			</NavLink>
+			{/* <div className="w-48"></div> */}
+		</div>
 	);
 }
