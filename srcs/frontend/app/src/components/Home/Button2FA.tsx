94ac35f156303cfa22cf612e1478acc2edf86c18
Adrian <adrian.mael.jung@gmail.com>
2023-08-20 00:29:25 +0200

    old components in comments

--- srcs/frontend/app/src/components/Home/Button2FA.tsx
+++ srcs/frontend/app/src/components/Home/Button2FA.tsx
@@ -1,15 +1,15 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-function ButtonSettings() {
-	const navigate = useNavigate();
-	function goTo2FA() {
-		navigate("/settings");
-	}
-	return (
-		<div>
-			<button onClick={goTo2FA}> Go to Settings </button>
-		</div>
-	);
-}
+// function ButtonSettings() {
+// 	const navigate = useNavigate();
+// 	function goTo2FA() {
+// 		navigate("/settings");
+// 	}
+// 	return (
+// 		<div>
+// 			<button onClick={goTo2FA}> Go to Settings </button>
+// 		</div>
+// 	);
+// }
 
-export default ButtonSettings;
+// export default ButtonSettings;
