21e25afde96413ff1a11478ec46544ee6679f2de
Adrian <adrian.mael.jung@gmail.com>
2023-08-28 05:34:58 +0200

    new search bar

--- srcs/frontend/app/src/components/NavBar/SearchUserInput.tsx
+++ srcs/frontend/app/src/components/NavBar/SearchUserInput.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import { FaSearch } from "react-icons/fa";
 
 export function SearchUserInput() {
 	const [search, setSearch] = useState("");
@@ -15,14 +16,17 @@ export function SearchUserInput() {
 	}
 	return (
 		<div className="flex items-center">
-			<input
-				className="px-2 py-1 mx-4 rounded-md w-52 h-12"
-				type="search"
-				placeholder="Search for a user..."
-				value={search}
-				onKeyDown={handleEnterKey}
-				onChange={handleSearch}
-			></input>
+			<div className="flex items-center h-14 bg-zinc-700 px-5 py-2 rounded-full">
+				<FaSearch className="w-5 h-5" />
+				<input
+					className=" px-2 w-48 bg-zinc-700 outline-none"
+					type="search"
+					placeholder="Search a user..."
+					value={search}
+					onKeyDown={handleEnterKey}
+					onChange={handleSearch}
+				></input>
+			</div>
 		</div>
 	);
 }
