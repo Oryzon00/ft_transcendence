23de0ae13fed749a69165d1e4ec823df3c9d326a
kodo <jesuiskodo@gmail.com>
2023-09-04 16:46:51 +0200

    polling done sensitive info leak n'oublie pas

--- srcs/frontend/app/src/components/Friends/FriendsList.tsx
+++ srcs/frontend/app/src/components/Friends/FriendsList.tsx
@@ -7,29 +7,41 @@ import {notifyError} from "../../utils/notify.ts";
 function FriendList () {
 	const [data, setData] = useState(null); // Utiliser un array
 	let listFriend = null;
- 	useEffect (() => {
-	    let url = apiAddress + "/user/friends/get";
-	    fetch (url, {
-		    method: "GET",
-		    headers: {
-			    Authorization: "Bearer " + getJwtTokenFromCookie(),
-			    "Content-Type": "application/json"
-		    },
-	    })
-		    .then(function (response) {
-			    if (!response.ok)
-				    throwErrorMessage(response);
-			    return response.json();
-		    })
-		    .then(function (result) {
+
+	function getFriends () {
+		let url = apiAddress + "/user/friends/get";
+		fetch (url, {
+			method: "GET",
+			headers: {
+				Authorization: "Bearer " + getJwtTokenFromCookie(),
+				"Content-Type": "application/json"
+			},
+		})
+			.then(function (response) {
+				if (!response.ok)
+					throwErrorMessage(response);
+				return response.json();
+			})
+			.then(function (result) {
+				console.log("polling...");
 				setData(result);
-		    })
-		    .catch(function () {
-			    notifyError("Error while getting friend");
-		    });
+			})
+			.catch(function () {
+				notifyError("Error while getting friend");
+			});
+	}
+
+ 	useEffect (() => {
+		getFriends();
+		const interval = window.setInterval(() => {
+			getFriends()
+		}, 1000);
+
+		return () => window.clearInterval(interval);
 	}, []);
+
 	 if (data !== null) {
-		 listFriend = data.friends.map((friend) => (
+		 listFriend = data.friends.map((friend :any) => (
 			 <li key={friend.id}>
 				 <div className="friends-friendlist-component">
 				        <img src={friend.image}></img>
