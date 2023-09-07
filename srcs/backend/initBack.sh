53cf47661b864bb8c11704a8932e0b0d13f61627
Adrian JUNG <adrian.mael.jung@gmail.com>
2023-09-07 19:04:59 +0200

    small modif script clear migrations

--- srcs/backend/initBack.sh
+++ srcs/backend/initBack.sh
@@ -4,12 +4,12 @@ rm -rf dist/
 rm -rf nodes_modules/
 rm -rf images/*
 
-# rm -rf src/prisma/migrations
+rm -rf src/prisma/migrations
 npm install
 
 # npx prisma db push --accept-data-loss
 npx prisma migrate dev --name init
-npx prisma generate
+# npx prisma generate
 
 #Start nest
 npm run start:dev & npx prisma studio
