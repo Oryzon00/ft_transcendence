bbfbe042eeb1394c2600f11e287dbf95099b5038
ko <lheurtev@made-f0Cr6s4.clusters.42paris.fr>
2023-09-26 16:44:06 +0200

    start game invite

--- srcs/backend/api/src/chat/chat.controller.ts
+++ srcs/backend/api/src/chat/chat.controller.ts
@@ -175,10 +175,18 @@ export class ChatController {
 
 	@Post("/fight")
 	async Fight(@GetUser() user: User, @Body() body: { id: string }) {
-		this.ChatService.message(user, {
-			channelId: body.id,
-			authorId: user.id,
-			content: "here for the fight"
-		});
+		if(!await this.ChatService.fight(user, body.id)) {
+			this.ChatService.message(user, {
+				channelId: body.id,
+				authorId: user.id,
+				content: "Ntm jveux jouer"
+			});
+		} else {
+			this.ChatService.message(user, {
+				channelId: body.id,
+				authorId: user.id,
+				content: "here for the fight /play"
+			});
+		}
 	}
 }
